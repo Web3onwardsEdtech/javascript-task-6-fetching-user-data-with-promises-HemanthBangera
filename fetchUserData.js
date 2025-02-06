6a5e430b60f01ad039d4cd557215d7deec2fa5a7
HemanthBangera <hemanthbangera843@gmail.com>
2025-02-06 18:20:43 +0530

    Completed task 6

--- fetchUserData.js
+++ fetchUserData.js
@@ -1,1 +1,22 @@
+function fetchUserData(){
+    return new Promise((resolve,reject) => {
+        fetch("https://jsonplaceholder.typicode.com/users/1")
+        .then((response) => {
+            if(!response.ok){
+                throw new Error('Network response: Failure')
+            }
+            else{
+                return response.json();
+            }
+        })
+        .then((info) => {
+            resolve({name: info.name, email: info.email});
+        })
+        .catch(error => {
+            reject('Error in fetching user data');
+        });
+        
+    });
+}
 
+fetchUserData().then(data => console.log(data)).catch(error => console.log(error));
